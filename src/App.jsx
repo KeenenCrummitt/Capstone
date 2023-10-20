04642a75023be7ab0040518f43d8c1e5deb47e80
Keenen Crummitt <theemobilegardener@gmail.com>
2023-10-19 18:01:52 -0600

    Portfolio
    Established divs for specific content to backgrounds white background and blue background. Added temporary image. Added flex to break into 4 sections in the hero box. Added css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,23 @@
-// function MyButton() {
-//   return <button>I'm a button</button>;
-// }
+import "./App.css";
 
-// export default function MyApp() {
-//   return (
-//     <div>
-//       <h1>Welcome to my app</h1>
-//       <MyButton />
-//     </div>
-//   );
-// }
-import { useState } from "react";
-
-function Square({ value, onSquareClick }) {
+export default function MyApp() {
   return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
+    <div className="hero-box">
+      <div className="hero-background">
+        <div className="hero-left"></div>
+        <div className="hero-right"></div>
+      </div>
+      <div className="hero-content">
+        <div className="credentials">
+          <h2>Frontend Developer</h2>
+          <button>Hire Me</button>
+          <p></p>
+          <p></p>
+        </div>
+        <div className="self-image">
+          <img src="/Portfolio-img.png" alt="selfie" />
+        </div>
+      </div>
+    </div>
   );
-}
-
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
-    }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
-
-  return (
-    <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
