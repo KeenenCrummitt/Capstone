b7d710cecd576d46b198770a96a51416d910a3f6
Keenen Crummitt <theemobilegardener@gmail.com>
2024-01-13 07:46:11 -0700

    update copy right year

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footerContainer text-white">
-        <p className="copyright">&copy; Keenen Crummitt 2023</p>
+        <p className="copyright">&copy; Keenen Crummitt 2024</p>
       </div>
     </footer>
   );
